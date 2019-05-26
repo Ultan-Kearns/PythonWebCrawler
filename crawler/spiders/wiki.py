1a76be07343bc08b1b4b369705f67cf67b44a20f
Ultan-kearns <ultankearns@gmail.com>
2019-05-26 23:23:21 +0100

    Added comments and tried working on recursively retrieving URLS

--- crawler/spiders/wiki.py
+++ crawler/spiders/wiki.py
@@ -1,20 +1,18 @@
 import scrapy
 
-
+urls = [
+    'https://en.wikipedia.org/wiki/Portal:History'
+]
 class wiki(scrapy.Spider):
     name = "wiki"
-
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'wiki-%s.html' % page
         with open(filename, 'wb') as f:
+            #write out the body
             f.write(response.body)
+        #Print out save message
         self.log('Saved file %s' % filename)
